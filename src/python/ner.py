4281796b0101a797f07a652e6fdef786c541a4ad
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2024-03-01 12:38:02 +0200

    Replace chars range with words range

--- src/python/ner.py
+++ src/python/ner.py
@@ -4,12 +4,10 @@ import sys, json, io
 
 import stanza
 
-#from mitie import *
-
-#MITIE_MODELS_PATH = "./MITIE-models/model.dat"
 #stanza.download('en')
 
 input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+
 
 def get_possible_ner_tags():
     return [
@@ -33,6 +31,31 @@ def get_possible_ner_tags():
         "WORK_OF_ART"
     ]
 
+
+def get_range(doc, ent):
+    start = 0
+    end = 0
+    for sent in doc.sentences:
+        if sent.tokens[-1].start_char < ent.start_char:
+            start += len(sent.tokens)
+        else:
+            for token in sent.tokens:
+                if token.start_char < ent.start_char:
+                    start = start + 1
+                else:
+                    break
+        if sent.tokens[-1].end_char < ent.end_char:
+            end += len(sent.tokens)
+        else:
+            for token in sent.tokens:
+                if token.end_char < ent.end_char:
+                    end = end + 1
+                else:
+                    break
+
+    return {"start": start, "end": end}
+
+
 if __name__=='__main__':
     nlp = stanza.Pipeline(lang='en', processors='tokenize,ner,lemma')
     input_json = None
@@ -53,8 +76,7 @@ if __name__=='__main__':
                 output = [dict(entity=' '.join([(token.lemma.title() if token.text.istitle() else token.lemma)
                                                 for sent in nlp(ent.text).sentences for token in sent.words]),
                                tag=ent.type,
-                               start_char=ent.start_char,
-                               end_char=ent.end_char
+                               range=get_range(doc, ent)
                                ) for ent in doc.ents]
                 output = {"request": input_json, "response": {"named_entities": output}}
         output_json = json.dumps(output, ensure_ascii=False).encode('utf-8')
